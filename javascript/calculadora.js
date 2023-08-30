4210ca7ae9e97ea12cab404bf28b4b0d1b85922b
lucascoelholucinda <Lucas.c.Lucinda@gmail.com>
2023-08-30 20:31:06 -0300

    melhorias no front-end e desenvolvimento da logica do backend para operacoes basicas

--- javascript/calculadora.js
+++ javascript/calculadora.js
@@ -1,7 +1,4 @@
-/* alert("teste") */
-
 $(document).ready(function () {
-
     $('.botao_calculadora').click(function (e) {
 
         let dados = document.querySelector(".texto_resultado").value
@@ -13,44 +10,222 @@ $(document).ready(function () {
             valores_calcular = document.querySelector(".texto_resultado").value + "" + e.target.value
             document.querySelector(".texto_resultado").value = valores_calcular
         }
-
-        /* var regexp = new RegExp(/[^a-zA-Z 0-9]+/g);
-        var n = regexp.exec(dados);
-       
-        if (n != null) {
-            re
-            console.log(n)
-        } */
     })
 
-    /*   $('.botao_calculadora_adicao').click(function (e) {
-         
-          let dados = e.target.value
-          let valor = document.querySelector(".texto_resultado").value
-          document.querySelector(".texto_resultado").value = valor + "" + dados
-          var regexp = new RegExp(/[\.*+?^${}()|[\]]/g);
-          var n = regexp.exec(valor);
-          if (n != null) {
-  
-              let novovalor = valor.replace("+", "");
-              console.log(valor)
-  
-              document.querySelector(".texto_resultado").value = novovalor + "" + dados
-          }
-  
-  
-      }) */
+    $('#botao_calculadora_adicao').click(function (e) {
+        let dados = document.querySelector(".texto_resultado").value
+        let validandopontuacao = dados.includes('.')
+        let validadorVazio = document.querySelector(".texto_resultado").value
+        if (validadorVazio === "") {
 
-    /*  $('.botao_calculadora').click(function (e) {
- 
-         let dados = document.querySelector(".texto_resultado").value
-         if (dados === "") {
-             let valores_calcular = e.target.value
-             document.querySelector(".texto_resultado").value = valores_calcular
-         } else {
-             valores_calcular = e.target.value + "" + document.querySelector(".texto_resultado").value
-             document.querySelector(".texto_resultado").value = valores_calcular
-             
-         }
-     }) */
+        } else {
+            if (document.getElementById("soma").value != "") {
+                document.getElementById("soma").style.backgroundColor = "#08f100f2"
+                let valor1Receber = document.getElementById("soma").value
+                let removendopontos = valor1Receber.replace(/,/g, "").replace(/\./g, "")
+                let alterarvalor1 = parseInt(removendopontos)
+                let valor2Receber = document.querySelector(".texto_resultado").value
+                let alterarvalor2 = parseInt(valor2Receber)
+                let total = alterarvalor1 + alterarvalor2
+                let resultado = total.toLocaleString('pt-BR')
+                document.getElementById("soma").value = resultado
+                document.querySelector(".texto_resultado").value = ""
+
+            } else {
+                if (document.getElementById("soma").value == "" && validandopontuacao == true) {
+                    document.getElementById("soma").value = document.querySelector(".texto_resultado").value
+                    document.getElementById("soma").style.backgroundColor = "#08f100f2"
+                    document.querySelector(".texto_resultado").value = ""
+                } else {
+                    document.getElementById("soma").style.backgroundColor = "#08f100f2"
+                    let configurandoparanumero = parseInt(dados)
+                    let resultado = configurandoparanumero.toLocaleString('pt-BR')
+                    document.getElementById("soma").value = resultado
+                    document.querySelector(".texto_resultado").value = ""
+                }
+            }
+        }
+
+
+    })
+
+
+    $('#botao_calculadora_subtracao').click(function (e) {
+        let dados = document.querySelector(".texto_resultado").value
+        let validandopontuacao = dados.includes('.')
+        let validadorVazio = document.querySelector(".texto_resultado").value
+        if (validadorVazio === "") {
+
+        } else {
+            if (document.getElementById("soma").value != "") {
+                document.getElementById("soma").style.backgroundColor = "#f10000f2"
+                let valor1Receber = document.getElementById("soma").value
+                let removendopontos = valor1Receber.replace(/,/g, "").replace(/\./g, "")
+                let alterarvalor1 = parseInt(removendopontos)
+                let valor2Receber = document.querySelector(".texto_resultado").value
+                let alterarvalor2 = parseInt(valor2Receber)
+                let total = alterarvalor1 - alterarvalor2
+                let resultado = total.toLocaleString('pt-BR')
+                document.getElementById("soma").value = resultado
+                document.querySelector(".texto_resultado").value = ""
+
+            } else {
+                if (document.getElementById("soma").value == "" && validandopontuacao == true) {
+                    document.getElementById("soma").value = document.querySelector(".texto_resultado").value
+                    document.getElementById("soma").style.backgroundColor = "#f10000f2"
+                    document.querySelector(".texto_resultado").value = ""
+                } else {
+                    document.getElementById("soma").style.backgroundColor = "#f10000f2"
+                    let configurandoparanumero = parseInt(dados)
+                    let resultado = configurandoparanumero.toLocaleString('pt-BR')
+                    document.getElementById("soma").value = resultado
+                    document.querySelector(".texto_resultado").value = ""
+                }
+            }
+        }
+
+
+    })
+
+
+    $('#botao_calculadora_multiplicacao').click(function (e) {
+        let dados = document.querySelector(".texto_resultado").value
+        let validandopontuacao = dados.includes('.')
+        let validadorVazio = document.querySelector(".texto_resultado").value
+        if (validadorVazio === "") {
+
+        } else {
+            if (document.getElementById("soma").value != "") {
+                document.getElementById("soma").style.backgroundColor = "#ffee00f2"
+                let valor1Receber = document.getElementById("soma").value
+                let removendopontos = valor1Receber.replace(/,/g, "").replace(/\./g, "")
+                let alterarvalor1 = parseInt(removendopontos)
+                let valor2Receber = document.querySelector(".texto_resultado").value
+                let alterarvalor2 = parseInt(valor2Receber)
+                let total = alterarvalor1 * alterarvalor2
+                let resultado = total.toLocaleString('pt-BR')
+                document.getElementById("soma").value = resultado
+                document.querySelector(".texto_resultado").value = ""
+
+            } else {
+                if (document.getElementById("soma").value == "" && validandopontuacao == true) {
+                    document.getElementById("soma").value = document.querySelector(".texto_resultado").value
+                    document.getElementById("soma").style.backgroundColor = "#ffee00f2"
+                    document.querySelector(".texto_resultado").value = ""
+                } else {
+                    document.getElementById("soma").style.backgroundColor = "#ffee00f2"
+                    let configurandoparanumero = parseInt(dados)
+                    let resultado = configurandoparanumero.toLocaleString('pt-BR')
+                    document.getElementById("soma").value = resultado
+                    document.querySelector(".texto_resultado").value = ""
+                }
+            }
+        }
+
+
+    })
+
+    $('#botao_calculadora_divisao').click(function (e) {
+        let dados = document.querySelector(".texto_resultado").value
+        let validandopontuacao = dados.includes('.')
+        let validadorVazio = document.querySelector(".texto_resultado").value
+        if (validadorVazio === "") {
+
+        } else {
+            if (document.getElementById("soma").value != "") {
+                document.getElementById("soma").style.backgroundColor = "#00f7fff2"
+                let valor1Receber = document.getElementById("soma").value
+                let removendopontos = valor1Receber.replace(/,/g, "").replace(/\./g, "")
+                let alterarvalor1 = parseInt(removendopontos)
+                let valor2Receber = document.querySelector(".texto_resultado").value
+                let alterarvalor2 = parseInt(valor2Receber)
+                let total = alterarvalor1 / alterarvalor2
+                let resultado = total.toLocaleString('pt-BR')
+                document.getElementById("soma").value = resultado
+                document.querySelector(".texto_resultado").value = ""
+
+            } else {
+                if (document.getElementById("soma").value == "" && validandopontuacao == true) {
+                    document.getElementById("soma").value = document.querySelector(".texto_resultado").value
+                    document.getElementById("soma").style.backgroundColor = "#00f7fff2"
+                    document.querySelector(".texto_resultado").value = ""
+                } else {
+                    document.getElementById("soma").style.backgroundColor = "#00f7fff2"
+                    let configurandoparanumero = parseInt(dados)
+                    let resultado = configurandoparanumero.toLocaleString('pt-BR')
+                    document.getElementById("soma").value = resultado
+                    document.querySelector(".texto_resultado").value = ""
+                }
+            }
+        }
+
+
+    })
+
+    $('#botao_calculadora_resultado').click(function (e) {
+        let cor = document.getElementById("soma").style.backgroundColor
+        if (document.querySelector(".texto_resultado").value == "" && document.getElementById("soma").value != "") {
+            document.querySelector(".texto_resultado").value = document.getElementById("soma").value
+            document.getElementById("soma").style.backgroundColor = "#ffffffff"
+            document.querySelector(".texto_resultado").value = document.getElementById("soma").value
+            document.getElementById("soma").value = ""
+        } else {
+            if (document.querySelector(".texto_resultado").value != "" && document.getElementById("soma").value == "") {
+
+            } else {
+                if (cor == "rgba(241, 0, 0, 0.95)") {
+                    let valor1Receber = document.getElementById("soma").value
+                    let removendopontos = valor1Receber.replace(/,/g, "").replace(/\./g, "")
+                    let valor1Alterado = parseInt(removendopontos)
+                    let valor2Receber = document.querySelector(".texto_resultado").value
+                    let valor2Alterado = parseInt(valor2Receber)
+                    let total = valor1Alterado - valor2Alterado
+                    let resultado = total.toLocaleString('pt-BR')
+                    document.getElementById("soma").style.backgroundColor = "#ffffffff"
+                    document.getElementById("soma").value = ""
+                    document.querySelector(".texto_resultado").value = resultado
+
+                } else {
+                    if (cor == "rgba(255, 238, 0, 0.95)") {
+                        let valor1Receber = document.getElementById("soma").value
+                        let removendopontos = valor1Receber.replace(/,/g, "").replace(/\./g, "")
+                        let valor1Alterado = parseInt(removendopontos)
+                        let valor2Receber = document.querySelector(".texto_resultado").value
+                        let valor2Alterado = parseInt(valor2Receber)
+                        let total = valor1Alterado * valor2Alterado
+                        let resultado = total.toLocaleString('pt-BR')
+                        document.getElementById("soma").style.backgroundColor = "#ffffffff"
+                        document.getElementById("soma").value = ""
+                        document.querySelector(".texto_resultado").value = resultado
+                    } else {
+                        if (cor === "rgba(0, 247, 255, 0.95)") {
+                            let valor1Receber = document.getElementById("soma").value
+                            let removendopontos = valor1Receber.replace(/,/g, "").replace(/\./g, "")
+                            let valor1Alterado = parseInt(removendopontos)
+                            let valor2Receber = document.querySelector(".texto_resultado").value
+                            let valor2Alterado = parseInt(valor2Receber)
+                            let total = valor1Alterado / valor2Alterado
+                            let resultado = total.toLocaleString('pt-BR')
+                            document.getElementById("soma").style.backgroundColor = "#ffffffff"
+                            document.getElementById("soma").value = ""
+                            document.querySelector(".texto_resultado").value = resultado
+                        } else {
+                            let valor1Receber = document.getElementById("soma").value
+                            let removendopontos = valor1Receber.replace(/,/g, "").replace(/\./g, "")
+                            let valor1Alterado = parseInt(removendopontos)
+                            let valor2Receber = document.querySelector(".texto_resultado").value
+                            let valor2Alterado = parseInt(valor2Receber)
+                            let total = valor1Alterado + valor2Alterado
+                            let resultado = total.toLocaleString('pt-BR')
+                            document.getElementById("soma").style.backgroundColor = "#ffffffff"
+                            document.getElementById("soma").value = ""
+                            document.querySelector(".texto_resultado").value = resultado
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+
 })
